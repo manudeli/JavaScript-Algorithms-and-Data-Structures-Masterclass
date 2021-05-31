d963a8a706cca03aafc15e962419db5dec8a5dff
flowithus <flowithusdev@gmail.com>
2021-05-31 16:19:23 +0900

    Exercise - areThereDuplicates Added another way Multiple Pointers

--- src/06. 100percentage OPTIONAL Challenges/04. Frequency Counter, Multiple Pointer - areThereDuplicates.js
+++ src/06. 100percentage OPTIONAL Challenges/04. Frequency Counter, Multiple Pointer - areThereDuplicates.js
@@ -5,18 +5,35 @@ arguments, and checks whether there are any duplicates among the arguments passe
 You can solve this using the frequency counter pattern OR the multiple pointers pattern.
 */
 
-function areThereDuplicates() {
-  const arrArguments = [...arguments];
-  const frequencyCounter = {};
-  let result = false;
-  arrArguments.map((argument) => {
-    frequencyCounter[argument] = (frequencyCounter[argument] || 0) + 1;
-    if (frequencyCounter[argument] >= 2) {
-      result = true;
-    }
-  });
+/* Way 1. Frequency Counter */
+// function areThereDuplicates() {
+//   const arrArguments = [...arguments];
+//   const frequencyCounter = {};
+//   let result = false;
+//   arrArguments.map((argument) => {
+//     frequencyCounter[argument] = (frequencyCounter[argument] || 0) + 1;
+//     if (frequencyCounter[argument] >= 2) {
+//       result = true;
+//     }
+//   });
 
-  return result;
+//   return result;
+// }
+
+/* Way 2. Multiple Pointers */
+function areThereDuplicates(...args) {
+  // Two pointers
+  args.sort((a, b) => a > b);
+  let start = 0;
+  let next = 1;
+  while (next < args.length) {
+    if (args[start] === args[next]) {
+      return true;
+    }
+    start++;
+    next++;
+  }
+  return false;
 }
 
 console.log(
