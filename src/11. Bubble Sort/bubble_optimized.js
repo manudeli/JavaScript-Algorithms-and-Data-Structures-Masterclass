046ec04003c742ec8b37c35e95ae4def02b15434
flowithus <flowithusdev@gmail.com>
2021-06-03 07:24:38 +0900

    Bubble Sort

---
+++ src/11. Bubble Sort/bubble_optimized.js
@@ -0,0 +1,20 @@
+// Optimized BubbleSort with noSwaps
+function bubbleSort(arr) {
+  var noSwaps;
+  for (var i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (var j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+bubbleSort([8, 1, 2, 3, 4, 6, 5, 7]);
