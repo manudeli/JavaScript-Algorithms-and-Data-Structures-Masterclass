b40b8d244dd19ec7d9ba0e3845bdb90b3d8755af
flowithus <flowithusdev@gmail.com>
2021-06-02 17:10:20 +0900

    Exercise - power

---
+++ src/08. Recursion Problem Set/10. power.js
@@ -0,0 +1,19 @@
+// Power
+/* 
+Write a function called power which accepts a base and an exponent. The function should
+return the power of the base to the exponent. This function should mimic the functionality of
+Math.pow() - do not worry about negative bases an exponents.
+*/
+
+let number = 0;
+
+function power(base, exponent) {
+  if (exponent === 0) return 1;
+  return base * power(base, exponent - 1);
+}
+
+console.log(
+  power(2, 0), // 1
+  power(2, 2), // 4
+  power(2, 4) // 16
+);
