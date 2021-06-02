b05cd21a032a281472bf63953c29f79ee415ccda
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-03 00:43:19 +0900

    Naive String Search Implementation

---
+++ src/10. Searching Algorithms/Naive String Search Implementation.js
@@ -0,0 +1,14 @@
+// Naive String Search Implementation
+
+function naiveSearch(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (long[i + j] !== short[j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+console.log(naiveSearch("lorie loled", "lol"));
