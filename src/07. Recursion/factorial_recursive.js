ed8de24484d8f7708c40dfbc0af4c5fc8382c2b4
flowithus <flowithusdev@gmail.com>
2021-06-02 15:36:00 +0900

    Factorial Recursive

---
+++ src/07. Recursion/factorial_recursive.js
@@ -0,0 +1,14 @@
+// function factorial(num) {
+//   let total = 1;
+//   for (let i = num; i > 1; i--) {
+//     total *= i;
+//   }
+//   return total;
+// }
+
+console.log(factorial(4));
+
+function factorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
+}
