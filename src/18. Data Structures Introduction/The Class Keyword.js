0e9d99065693cfc0cd63aff44770749becd2b585
flowithus <flowithusdev@gmail.com>
2021-06-04 23:21:18 +0900

    Add instance methods in Class

--- src/18. Data Structures Introduction/The Class Keyword.js
+++ src/18. Data Structures Introduction/The Class Keyword.js
@@ -3,11 +3,32 @@ class Student {
     this.firstName = firstName;
     this.lastName = lastName;
     this.grade = year;
+    this.tardies = 0;
+    this.scores = [];
+  }
+  fullName() {
+    return `Your full name is ${this.firstName} ${this.lastName}`;
+  }
+  markLate() {
+    this.tardies += 1;
+    return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
+  }
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
+  }
+  calculateAverage() {
+    let sum = this.scores.reduce(function (a, b) {
+      return a + b;
+    });
+    return sum / this.scores.length;
   }
 }
 
 let firstStudent = new Student("Jonghyeon", "Ko", 3);
 let secondStudent = new Student("Cheolsoo", "Kim", 4);
+
+console.log(firstStudent.markLate());
 
 console.log(firstStudent);
 console.log(secondStudent);
@@ -16,3 +37,11 @@ console.log(firstStudent.firstName);
 firstStudent.firstName = "Jonkyky";
 
 console.log(firstStudent.firstName);
+console.log(firstStudent.fullName());
+
+secondStudent.addScore(92);
+secondStudent.addScore(97);
+secondStudent.addScore(30);
+secondStudent.addScore(70);
+console.log(secondStudent.scores);
+console.log(secondStudent.calculateAverage());
