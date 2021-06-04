0387cfd88ba6d13a87d9e78db2b5e0a70dabc462
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-04 18:36:56 +0900

    Quick Sort

---
+++ src/16. Quick Sort/quickSort.js
@@ -0,0 +1,33 @@
+function pivot(arr, start = 0, end = arr.length + 1) {
+  function swap(array, i, j) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    // left
+    quickSort(arr, left, pivotIndex - 1);
+    // right
+    quickSort(arr, pivotIndex + 1, right);
+    console.log(pivotIndex, arr);
+  }
+  return arr;
+}
+
+console.log(quickSort([4, 6, 9, 1, 2, 5, 2, 23, 24, 2, 4234, 24, 234, 234]));
