61679d182fcbe6aa468755d3a0ce97457e7e2bb5
flowithus <flowithusdev@gmail.com>
2021-06-06 15:11:35 +0900

    Doubly Linked Lists :: Push

---
+++ src/20. Doubly Linked Lists/Doubly_Linked_Lists.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+let first = new Node(12);
+
+first.next = new Node(13);
+first.next.prev = first;
+console.log(first);
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+listFirst = new DoublyLinkedList();
+
+listFirst.push(14);
+listFirst.push(15);
+listFirst.push(16);
+console.log(listFirst);
