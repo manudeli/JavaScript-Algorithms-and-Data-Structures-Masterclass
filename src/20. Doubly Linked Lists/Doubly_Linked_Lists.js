8de19d3e94f495d8b9ef681ccacb6f0de9d32b47
flowithus <flowithusdev@gmail.com>
2021-06-06 17:15:02 +0900

    Doubly Linked Lists :: Shift

--- src/20. Doubly Linked Lists/Doubly_Linked_Lists.js
+++ src/20. Doubly Linked Lists/Doubly_Linked_Lists.js
@@ -45,6 +45,20 @@ class DoublyLinkedList {
     this.length--;
     return poppedNode;
   }
+  shift() {
+    if (this.length === 0) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
 }
 
 listFirst = new DoublyLinkedList();
