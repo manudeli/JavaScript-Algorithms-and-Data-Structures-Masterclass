68aee7eff1dbe2230cdeb8136bd9da0a29594cee
flowithus <flowithusdev@gmail.com>
2021-06-08 18:43:51 +0900

    Hash Table :: Set and Get

---
+++ src/24. Hash Tables/Hash Function.js
@@ -0,0 +1,45 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+}
+
+let ht = new HashTable();
+console.log(ht.set("hello world", "goodbye!!"));
+console.log(ht.set("dogs", "are cool"));
+console.log(ht.set("cats", "are fine"));
+console.log(ht.set("i love", "pizza"));
+console.log(ht.set("hi", "bye"));
+
+console.log(ht);
+console.log(ht.get("hi"));
+console.log(ht.get("i love"));
