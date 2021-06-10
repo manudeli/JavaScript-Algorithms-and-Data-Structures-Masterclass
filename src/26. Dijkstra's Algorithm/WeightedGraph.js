87bb86c3a1fd5805a63334c2ac23ccca15118fe7
flowithus <flowithusdev@gmail.com>
2021-06-10 15:02:42 +0900

    Weighted Graph :: Add Priority Queue

--- src/26. Dijkstra's Algorithm/WeightedGraph.js
+++ src/26. Dijkstra's Algorithm/WeightedGraph.js
@@ -1,3 +1,18 @@
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
 class WeightedGraph {
   constructor() {
     this.adjacencyList = {};
@@ -9,13 +24,22 @@ class WeightedGraph {
     this.adjacencyList[vertex1].push({ node: vertex2, weight });
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
   }
+  Dijkstra(start, finish) {}
 }
 
 let graph = new WeightedGraph();
 graph.addVertex("A");
 graph.addVertex("B");
 graph.addVertex("C");
-graph.addEdge("A", "B", 9);
-graph.addEdge("A", "C", 5);
-graph.addEdge("B", "C", 7);
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+graph.addEdge("A", "B", 4);
+graph.addEdge("A", "C", 2);
+graph.addEdge("B", "E", 3);
+graph.addEdge("C", "D", 2);
+graph.addEdge("C", "F", 4);
+graph.addEdge("D", "E", 3);
+graph.addEdge("D", "F", 1);
+graph.addEdge("E", "F", 1);
 console.log(graph.adjacencyList);
